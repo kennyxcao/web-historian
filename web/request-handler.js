97c5dc5285046e518cd937d1c6351385001a4abf
Hack Reactor Student <student@hackreactor.com>
2017-09-15 14:47:50 -0700

    Implement GET request to redirect to index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,28 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var querystring = require('querystring');
+var helpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('Request Method: ' + req.method + ' Request URL:' + req.url);
+  
+  if (req.method === 'GET') {
+    if (req.url = '/') {
+      
+      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+        if (err) {
+          res.writeHead(404, helpers.headers);
+          res.end();
+        } else {
+          res.writeHead(200, helpers.headers);
+          res.end(data);
+        }
+      });
+    }
+  }
+
+  //res.end(archive.paths.list);
 };
