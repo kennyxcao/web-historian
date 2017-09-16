1900d1784d6a2a5671295515844dde2db7e8d646
Hack Reactor Student <student@hackreactor.com>
2017-09-15 20:07:40 -0700

    Implement cron using node-cron to run htmlfetchers every min

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,20 +1,23 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
-
+// * * * * * /Users/student/.nvm/versions/node/v6.9.5/bin/node /Users/student/code/hrsf82-web-historian/workers/htmlfetcher.js
 var archive = require('../helpers/archive-helpers');
+var cron = require('node-cron');
 
-archive.readListOfUrls(function(urls) {
-  var fetchURLs = [];
-  
-  for (let i = 0; i < urls.length; i++) {
-    var downloadURL = function(exist) {
-      if (!exist) {
-        fetchURLs.push(urls[i]);
-      }
-      if (i === urls.length - 1) {
-        archive.downloadUrls(fetchURLs);
-      }
-    };        
-    archive.isUrlArchived(urls[i], downloadURL, i);
-  }
+cron.schedule('* * * * *', function() {
+  archive.readListOfUrls(function(urls) {
+    var fetchURLs = [];
+    
+    for (let i = 0; i < urls.length; i++) {
+      var downloadURL = function(exist) {
+        if (!exist) {
+          fetchURLs.push(urls[i]);
+        }
+        if (i === urls.length - 1) {
+          archive.downloadUrls(fetchURLs);
+        }
+      };        
+      archive.isUrlArchived(urls[i], downloadURL, i);
+    }
+  });
 });
