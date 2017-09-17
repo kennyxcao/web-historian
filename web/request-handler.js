d1b6e9ad01bc31a1493e5b32257db8b4cde44819
Hack Reactor Student <student@hackreactor.com>
2017-09-16 17:18:22 -0700

    Implement working solution using promises

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,18 +6,23 @@ var helpers = require('./http-helpers');
 var fs = require('fs');
 // require more modules/folders here!
 
+var sendResponse = function(res, statusCode, path) {
+  res.writeHead(statusCode, helpers.headers);
+  var fileStream = fs.createReadStream(path);
+  fileStream.pipe(res); 
+};
+
 exports.handleRequest = function (req, res) {
   console.log('Request Method: ' + req.method + ' Request URL:' + req.url);
   
   if (req.method === 'GET') {
     var filePath = req.url === '/' ? archive.paths.siteAssets + '/index.html' : archive.paths.archivedSites + req.url;
-
     fs.readFile(filePath, function(err, data) {
       res.writeHead(err ? 404 : 200, helpers.headers);
       res.end(data);
     });
   }
-
+  // Working promise solution
   if (req.method === 'POST') {
     var data = '';
     req.on('data', function (chunk) {
@@ -26,20 +31,17 @@ exports.handleRequest = function (req, res) {
     req.on('end', function () {
       var url = querystring.parse(data).url;
       
-      archive.isUrlInList(url, function(exist) {
+      archive.isUrlArchivedAsync(url)
+      .then(function() {
+        sendResponse(res, 302, archive.paths.archivedSites + '/' + url);  
+      })
+      .catch(function(err) {
+        sendResponse(res, 500, archive.paths.siteAssets + '/loading.html');
+        return archive.isUrlInListAsync(url);
+      })
+      .then(function(exist) {
         if (!exist) {
-          archive.addUrlToList(url + '\n', function(err) {
-            res.writeHead(err ? 500 : 302, helpers.headers);
-            var fileStream = fs.createReadStream(archive.paths.siteAssets + '/loading.html');
-            fileStream.pipe(res); 
-          });
-        } else {
-          archive.isUrlArchived(url, function(exist) {
-            res.writeHead(exist ? 302 : 500, helpers.headers);            
-            var redirect = exist ? archive.paths.archivedSites + '/' + url : archive.paths.siteAssets + '/loading.html';
-            var fileStream = fs.createReadStream(redirect);
-            fileStream.pipe(res);
-          });
+          archive.addUrlToListAsync(url + '\n');
         }
       });
     });
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,6 +3,7 @@
 // * * * * * /Users/student/.nvm/versions/node/v6.9.5/bin/node /Users/student/code/hrsf82-web-historian/workers/htmlfetcher.js
 var archive = require('../helpers/archive-helpers');
 var cron = require('node-cron');
+var Promise = require('bluebird');
 
 cron.schedule('* * * * *', function() {
   var fetchURLs = [];
@@ -21,7 +22,6 @@ cron.schedule('* * * * *', function() {
 // cron.schedule('* * * * *', function() {
 //   archive.readListOfUrls(function(urls) {
 //     var fetchURLs = [];
-    
 //     for (let i = 0; i < urls.length; i++) {
 //       var downloadURL = function(exist) {
 //         if (!exist) {
